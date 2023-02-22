ade76f84d6efd41e825f0d1b7663924e06e129f9
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-02-22 09:03:55 -0800

    player shoot fix

--- last one standing/Assets/Scripts/ActiveScripts/PlayerShoot.cs
+++ last one standing/Assets/Scripts/ActiveScripts/PlayerShoot.cs
@@ -36,6 +36,7 @@ public class PlayerShoot : MonoBehaviour
                 GetComponent<AudioSource>().PlayOneShot(shootSound);
                 timer = 0;
                 currentAmmo--;
+                Debug.Log("Successful Shot");
 
             }
         }
